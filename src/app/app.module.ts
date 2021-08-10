ee20db345c9b3d2b848e2636c0095c4454c31b9b
JUAN CARLOS ORTIZ REALES <juancortiz@unicesar.edu.co>
2021-08-10 16:58:59 -0500

    Holis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {HttpClientModule} from '@angular/common/http';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, FileOpener,AndroidPermissions],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import { Plugins,  } from '@capacitor/core';
+
+const { Filesystem } = Plugins;
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { Directory } from '@capacitor/filesystem';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  pdfObject: any;
+  constructor(
+    private plt: Platform
+    , private fileOpener: FileOpener
+    , private androidPermissions: AndroidPermissions
+    ) { }
 
+  ngOnInit() {
+
+  }
+
+  createPdf() {
+    const docDefinitions = {
+      watermark: { text: 'AGRORGANICO SEMILLAS', color: 'green', italic: true },
+      content: [
+        {
+          columns: [
+            {
+              text: 'Holis',
+              alignment: 'center'
+            }
+        ]
+        }
+
+
+      ]
+    }
+
+    this.pdfObject = pdfMake.createPdf(docDefinitions);
+    console.log("Generado");
+
+  }
+
+  downloadPdf() {
+
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then(
+      result => console.log('Has permission?',result.hasPermission),
+      err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE)
+    );
+
+    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE]);
+    if (this.plt.is('cordova')) {
+      this.pdfObject.getBase64(async (data) => {
+        try {
+          let path = `pdf/myletter_${Date.now()}.pdf`;
+
+         await Filesystem.writeFile({
+            path: `pdf/myletter_${Date.now()}.pdf`,
+            data: data,
+            directory: Directory.Documents,
+            recursive: true
+          })
+         // this.fileOpener.open(`${result.uri}`, 'application/pdf');
+        } catch (error) {
+          console.error('Unable to write file', error);
+        }
+      });
+    } else {
+      this.pdfObject.download();
+    }
+  }
+
+
+  openPdf(){
+    this.pdfObject.open();
+  }
 }
